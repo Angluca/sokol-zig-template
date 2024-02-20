d6299eeb5895424d1368c7816eee6ac59c24ba92
Pixeller <156404+Angluca@users.noreply.github.com>
2024-02-20 18:45:25 +0800

    Update

--- examples/shapes-transform.zig
+++ examples/shapes-transform.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const sk = @cImport({
     @cInclude("sokol.h");
-    @cInclude("shaders/shapes.glsl.h");
+    @cInclude("shaders/shapes-transform.glsl.h");
 });
 const print = std.debug.print;
 const assert = std.debug.assert;
@@ -11,25 +11,10 @@ var bind: sk.sg_bindings = .{};
 var pass_action: sk.sg_pass_action = .{};
 var rx: f32 = 0;
 var ry: f32 = 0;
-const Shape = struct {
-    pos: sk.hmm_vec3 = .{},
-    draw: sk.sshape_element_range_t = .{},
-};
-const NUM_SHAPES = 5;
-const SE = enum(u3) {
-    BOX,
-    PLANE,
-    SPHERE,
-    CYLINDER,
-    TORUS,
-};
-inline fn setoi(se: SE) u32 {
-    return @intFromEnum(se);
-}
-var shapes: [NUM_SHAPES]Shape = undefined;
 var vs_params: sk.vs_params_t = .{};
 var vbuf: sk.sg_buffer = .{};
 var ibuf: sk.sg_buffer = .{};
+var elms: sk.sshape_element_range_t = undefined;
 
 export fn init() void {
     sk.sg_setup(&sk.sg_desc{
@@ -57,61 +42,59 @@ export fn init() void {
     pip_desc.layout.attrs[3] = sk.sshape_color_vertex_attr_state();
     pip = sk.sg_make_pipeline(&pip_desc);
 
-    // shape positions
-    shapes[setoi(.BOX)].pos = sk.HMM_Vec3(-1.0, 1, 0);
-    shapes[setoi(.PLANE)].pos = sk.HMM_Vec3(1.0, 1, 0);
-    shapes[setoi(.SPHERE)].pos = sk.HMM_Vec3(-2.0, -1, 0);
-    shapes[setoi(.CYLINDER)].pos = sk.HMM_Vec3(2.0, -1, 0);
-    shapes[setoi(.TORUS)].pos = sk.HMM_Vec3(0, -1, 0);
     const vertices = [_]sk.sshape_vertex_t{.{}} ** (6 * 1024);
     const indices = [_]u16{0} ** (16 * 1024);
     var buf = sk.sshape_buffer_t {
         .vertices = .{.buffer = sk.SSHAPE_RANGE(vertices)},
         .indices = .{.buffer = sk.SSHAPE_RANGE(indices)},
     };
+    const box_transform = sk.HMM_Translate(sk.HMM_Vec3(-1,0,1));
+    const sphere_transform = sk.HMM_Translate(sk.HMM_Vec3(1,0,1));
+    const cylinder_transform = sk.HMM_Translate(sk.HMM_Vec3(-1,0,-1));
+    const torus_transform = sk.HMM_Translate(sk.HMM_Vec3(1,0,-1));
+
     buf = sk.sshape_build_box(&buf, &sk.sshape_box_t{
         .width = 1.0,
         .height = 1.0,
         .depth = 1.0,
         .tiles = 10,
         .random_colors = true,
+        .transform = sk.sshape_mat4(&box_transform.Elements[0][0])
     });
-    shapes[setoi(.BOX)].draw = sk.sshape_element_range(&buf);
-    buf = sk.sshape_build_plane(&buf, &sk.sshape_plane_t{
-        .width = 1.0,
-        .depth = 1.0,
-        .tiles = 10,
-        .random_colors = true,
-    });
-    shapes[setoi(.PLANE)].draw = sk.sshape_element_range(&buf);
     buf = sk.sshape_build_sphere(&buf, &sk.sshape_sphere_t{
+        .merge = true,
         .radius = 0.75,
         .slices = 36,
         .stacks = 20,
         .random_colors = true,
+        .transform = sk.sshape_mat4(&sphere_transform.Elements[0][0])
     });
-    shapes[setoi(.SPHERE)].draw = sk.sshape_element_range(&buf);
     buf = sk.sshape_build_cylinder(&buf, &sk.sshape_cylinder_t{
+        .merge = true,
         .radius = 0.5,
         .height = 1.5,
         .slices = 36,
         .stacks = 10,
         .random_colors = true,
+        .transform = sk.sshape_mat4(&cylinder_transform.Elements[0][0])
     });
-    shapes[setoi(.CYLINDER)].draw = sk.sshape_element_range(&buf);
     buf = sk.sshape_build_torus(&buf, &sk.sshape_torus_t{
+        .merge = true,
         .radius = 0.5,
         .ring_radius = 0.3,
         .rings = 36,
         .sides = 18,
         .random_colors = true,
+        .transform = sk.sshape_mat4(&torus_transform.Elements[0][0])
     });
-    shapes[setoi(.TORUS)].draw = sk.sshape_element_range(&buf);
     assert(buf.valid);
 
+    // extract element range for sg_draw()
+    elms = sk.sshape_element_range(&buf);
+
+    // and finally create the vertex- and index-buffer
     vbuf = sk.sg_make_buffer(&sk.sshape_vertex_buffer_desc(&buf));
     ibuf = sk.sg_make_buffer(&sk.sshape_index_buffer_desc(&buf));
-
     bind.vertex_buffers[0] = vbuf;
     bind.index_buffer = ibuf;
 
@@ -138,17 +121,14 @@ export fn frame() void {
     rx += 1.0 * t; ry += 2.0 * t;
     const rxm = sk.HMM_Rotate(rx, sk.HMM_Vec3(1.0, 0.0, 0.0));
     const rym = sk.HMM_Rotate(ry, sk.HMM_Vec3(0.0, 1.0, 0.0));
-    const rm = sk.HMM_MultiplyMat4(rxm, rym);
+    const model = sk.HMM_MultiplyMat4(rxm, rym);
+    vs_params.mvp = sk.HMM_MultiplyMat4(view_proj, model);
 
     sk.sg_begin_default_pass(&pass_action, @intFromFloat(w), @intFromFloat(h));
     sk.sg_apply_pipeline(pip);
     sk.sg_apply_bindings(&bind);
-    for(0..NUM_SHAPES)|i| {
-        const model = sk.HMM_MultiplyMat4(sk.HMM_Translate(shapes[i].pos), rm);
-        vs_params.mvp = sk.HMM_MultiplyMat4(view_proj, model);
-        sk.sg_apply_uniforms(sk.SG_SHADERSTAGE_VS, sk.SLOT_vs_params, &sk.SG_RANGE(vs_params));
-        sk.sg_draw(shapes[i].draw.base_element, shapes[i].draw.num_elements, 1);
-    }
+    sk.sg_apply_uniforms(sk.SG_SHADERSTAGE_VS, sk.SLOT_vs_params, &sk.SG_RANGE(vs_params));
+    sk.sg_draw(elms.base_element, elms.num_elements, 1);
     sk.sdtx_draw();
     sk.sg_end_pass();
     sk.sg_commit();
@@ -181,7 +161,7 @@ pub fn main() void {
             .width = 400,
             .height = 300,
             .sample_count = 4,
-            .window_title = "shapes (sokol-app)",
+            .window_title = "shapes-transform (sokol-app)",
             .icon = .{ .sokol_default = true, },
             .logger = .{ .func = sk.slog_func, },
         },
